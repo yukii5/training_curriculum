61022a925c1da31e974b0d330de3600b0917f099
yukii5 <assault03@gmail.com>
2020-09-26 21:56:55 +0900

    3.11

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,7 +2,7 @@ class CalendarsController < ApplicationController
 
   # １週間のカレンダーと予定が表示されるページ
   def index
-    getWeek
+    getWkee
     @plan = Plan.new
   end
 
@@ -15,6 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
+    bunding.pry
     params.require(:calendars).permit(:date, :plan)
   end
 
